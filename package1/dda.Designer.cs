64d590638eab75648f6a5359eda147d249dd9418
Your Name <you@example.com>
2021-06-24 23:19:40 +0200

    commit

---
+++ package1/dda.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace package1
+{
+    partial class dda
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.text4 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.Info;
+            this.panel.Location = new System.Drawing.Point(353, 2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(444, 264);
+            this.panel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DDA Algoritm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(12, 42);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(62, 23);
+            this.text1.TabIndex = 2;
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(12, 120);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(62, 23);
+            this.text3.TabIndex = 3;
+            // 
+            // text4
+            // 
+            this.text4.Location = new System.Drawing.Point(159, 120);
+            this.text4.Name = "text4";
+            this.text4.Size = new System.Drawing.Size(62, 23);
+            this.text4.TabIndex = 4;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(159, 42);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(62, 23);
+            this.text2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Y1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Y2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text4);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.TextBox text4;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ package1/dda.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace package1
+{
+    public partial class dda : Form
+    {
+        public dda()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(text1.Text);
+            int y1 = Convert.ToInt32(text2.Text);
+
+            int x2 = Convert.ToInt32(text3.Text);
+            int y2 = Convert.ToInt32(text4.Text);
+            lineDDA(x1, y1, x2, y2);
+
+
+
+        }
+        void lineDDA(int x0, int y0, int xEnd, int yEnd)
+        {
+            int dx = xEnd - x0, dy = yEnd - y0, steps, k;
+            float xIncrement, yIncrement, x = x0, y = y0;
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+                steps = Math.Abs(dx);
+            else
+                steps = Math.Abs(dy);
+            xIncrement = (float)(dx) / (float)(steps);
+            yIncrement = (float)(dy) / (float)(steps);
+
+            var brush = Brushes.Black;
+            var g = panel.CreateGraphics();
+
+            g.FillRectangle(brush, x, y, 2, 2);
+            for (k = 0; k < steps; k++)
+            {
+                x += xIncrement;
+                y += yIncrement;
+                g.FillRectangle(brush, x, y, 2, 2);
+            }
+        }
+
+
+    }
+}
